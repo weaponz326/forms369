d6e27a2354af7b6436833d32d88a33717513c2ba
Ralph Marvin Addo <ralph.addo@thegitlab.com>
2019-11-20 14:03:39 +0000

    client form searching

--- resources/forms-ui/src/app/pages/dashboard/client/client-list-forms-page/client-list-forms-page.component.ts
+++ resources/forms-ui/src/app/pages/dashboard/client/client-list-forms-page/client-list-forms-page.component.ts
@@ -3,6 +3,7 @@ import * as _ from 'lodash';
 import { Router } from '@angular/router';
 import { FormBuilderService } from 'src/app/services/form-builder/form-builder.service';
 import { FormsService } from 'src/app/services/forms/forms.service';
+import { ClientService } from 'src/app/services/client/client.service';
 
 @Component({
   selector: 'app-client-list-forms-page',
@@ -10,19 +11,20 @@ import { FormsService } from 'src/app/services/forms/forms.service';
   styleUrls: ['./client-list-forms-page.component.css']
 })
 export class ClientListFormsPageComponent implements OnInit {
-  query: string;
   company: any;
+  query: string;
   hasData: boolean;
   loading: boolean;
   hasMore: boolean;
   hasError: boolean;
+  foundNoForm: boolean;
   isConnected: boolean;
   formsList: Array<any>;
 
   constructor(
     private router: Router,
     private formService: FormsService,
-    private formBuilderService: FormBuilderService
+    private clientService: ClientService
   ) {
     this.formsList = [];
     this.company = history.state.company;
@@ -37,10 +39,88 @@ export class ClientListFormsPageComponent implements OnInit {
     this.router.navigateByUrl('/client/form_entry/' + form.form_code, { state: { form: form }});
   }
 
-  search(event: any) {
-    if (_.isUndefined(this.query) || _.isEmpty(this.query)) {
+  searchByFormCode() {
+    this.loading = true;
+    this.clientService.findFormsByCode(this.query).then(
+      forms => {
+        if (forms.length == 0) {
+          this.loading = false;
+          this.foundNoForm = true;
+        }
+        else {
+          this.loading = false;
+          this.foundNoForm = false;
+          _.forEach(forms, (form) => {
+            this.formsList.push(form);
+          });
+        }
+      },
+      err => {
+        this.hasError = true;
+        this.loading = false;
+      }
+    );
+  }
+
+  searchByFormName() {
+    this.loading = true;
+    this.clientService.findFormsByName(this.query).then(
+      forms => {
+        if (forms.length == 0) {
+          this.loading = false;
+          this.foundNoForm = true;
+        }
+        else {
+          this.loading = false;
+          this.foundNoForm = false;
+          _.forEach(forms, (form) => {
+            this.formsList.push(form);
+          });
+        }
+      },
+      err => {
+        this.hasError = true;
+        this.loading = false;
+      }
+    );
+  }
+
+  search(e: KeyboardEvent) {
+    if (e.key == 'Enter') {
+      // we need to know whether the user is searching by a form code
+      // or the user is searching by a form name.
+      // First, check if its a form code.
+      console.log(this.query);
+      this.hasError = false;
+      this.formsList = [];
+      if (/\d/.test(this.query)) {
+        if (this.query.length == 6) {
+          // search fby form code, based on the input
+          // the user might be searching by a form code.
+          console.log('searching by form code');
+          this.searchByFormCode();
+        }
+        else {
+          // the input contains a number but is more than 6 characters
+          // in lenght, this might be a form name.
+          console.log('searching by form name');
+          this.searchByFormName();
+        }
+      }
+      else {
+        // since all our form codes includes digits, and this
+        // users input doesnt include a digit, search by form name.
+        console.log('searching by form name last');
+        this.searchByFormName();
+      }
     }
     else {
+      if (this.foundNoForm && this.query.length == 0) {
+        this.hasData = true;
+        this.foundNoForm = false;
+        console.log('hererereererere');
+        this.getAllForms();
+      }
     }
   }
 
