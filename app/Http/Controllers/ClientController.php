dc90c3c19ce4bc379ac5629d0bfe7099a376259a
Warihana <thegitlab@TheGitLabs-iMac.local>
2020-01-22 03:56:29 -0800

    password reset implemented

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -706,7 +706,7 @@ class ClientController extends Controller
         ->delete(); 
  
         if($deleteattachements){
-            unlink(storage_path('attachments/'.$name));
+            unlink(storage_path('app/public/attachments/'.$name));
             $message = "Ok";
         }
 
